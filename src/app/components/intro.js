22ff48199a3e0e355527829fd9571c2a79490337
charlesphu <charlesphu18@gmail.com>
2025-06-28 23:34:08 -0700

    mui box to tailwind for intro

--- src/app/components/intro.js
+++ src/app/components/intro.js
@@ -3,81 +3,64 @@ import { AppBar, Button, Divider, Stack, Typography } from "@mui/material";
 import { Iconbar } from "./icons";
 export default function Intro() {
   return (
-    <Box sx={{ width: "100%" }}>
-      <Box
-        sx={{
-          minHeight: "100vh",
-          // bgcolor: "#f4f4f5",
-          bgcolor: "#CFD0DD",
-          display: "flex",
-          flexDirection: "column",
-        }}
+    <div className="h-screen bg-[#CFD0DD] flex flex-col justify-center items-center">
+      <Typography
+        variant="h1"
+        sx={{ color: "#543A14", fontFamily: "Merriweather" }}
       >
-        {/* <AppBar
+        Charles Phu
+      </Typography>
+      <Typography variant="h2" sx={{ color: "#543A14" }}>
+        Software Engineer
+      </Typography>
+      <Divider
+        sx={{
+          bg: "#F0BB78",
+          width: "50%",
+          margin: "2rem 0",
+        }}
+      />
+      <Iconbar />
+    </div>
+  );
+}
+
+// this was an appbar before
+{
+  /* <AppBar
           position="static"
           sx={{
             height: "10%",
             minHeight: "75px",
             bgcolor: "white",
             justifyContent: "center",
-          }}
-        >
-          <Stack
+            }}
+            >
+            <Stack
             direction="row"
             spacing={10}
             sx={{ justifyContent: "center", height: "100%" }}
-          >
-            <Button
-              variant="text"
-              sx={{ height: "100%", display: "flex", alignItems: "center" }}
             >
-              Home
+            <Button
+            variant="text"
+            sx={{ height: "100%", display: "flex", alignItems: "center" }}
+            >
+            Home
             </Button>
             <Divider orientation="vertical" flexItem />
             <Button
-              variant="text"
-              sx={{ height: "100%", display: "flex", alignItems: "center" }}
+            variant="text"
+            sx={{ height: "100%", display: "flex", alignItems: "center" }}
             >
-              About Me
+            About Me
             </Button>
             <Divider orientation="vertical" flexItem />
             <Button
-              variant="text"
-              sx={{ height: "100%", display: "flex", alignItems: "center" }}
+            variant="text"
+            sx={{ height: "100%", display: "flex", alignItems: "center" }}
             >
-              Contact
+            Contact
             </Button>
-          </Stack>
-        </AppBar> */}
-
-        <Box
-          sx={{
-            flex: 1,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography
-            variant="h1"
-            sx={{ color: "#543A14", fontFamily: "Merriweather" }}
-          >
-            Charles Phu
-          </Typography>
-          <Typography variant="h2" sx={{ color: "#543A14" }}>
-            Software Engineer
-          </Typography>
-          <Divider
-            sx={{
-              bg: "#F0BB78",
-              width: "50%",
-              margin: "2rem 0",
-            }}
-          />
-          <Iconbar />
-        </Box>
-      </Box>
-    </Box>
-  );
+            </Stack>
+            </AppBar> */
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,4 @@
 import "./globals.css";
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+  return <html lang="en">{children}</html>;
 }
