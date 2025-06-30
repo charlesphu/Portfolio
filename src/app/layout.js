f4908533922abddc99988161bdabf1c831b37d9d
charlesphu <charlesphu18@gmail.com>
2025-06-29 23:48:11 -0700

    hydration error fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,8 @@
 import "./globals.css";
 export default function RootLayout({ children }) {
-  return <html lang="en">{children}</html>;
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,35 @@
+"use client";
 import React from "react";
 import Intro from "./components/intro";
 import AboutMe from "./components/aboutMe";
 import Contact from "./components/contact";
 import Projects from "./components/projects";
 import { Divider } from "@mui/material";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+// import {  } from "gsap/";
 export default function Home() {
+  const fadeRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(fadeRef.current, {
+      opacity: 1,
+      duration: 4,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: fadeRef.current,
+        start: "top 80%", // when top of section hits 80% of viewport
+        toggleActions: "play none none reverse",
+      },
+    });
+  }, []);
+
   return (
     // <div className="h-screen bg-yellow-500"></div>
-    <body style={{ height: "100%", margin: 0 }}>
+    <div className="h-full">
       <div className="hscreen bg-black"></div>
       <Intro />
       <Divider
@@ -15,7 +37,7 @@ export default function Home() {
           borderWidth: "1px",
         }}
       />
-      <AboutMe />
+      <AboutMe ref={fadeRef} />
       <Divider
         sx={{
           borderWidth: "1px",
@@ -28,6 +50,6 @@ export default function Home() {
         }}
       />
       <Contact />
-    </body>
+    </div>
   );
 }
