364923a8687be3c5fd54e12d04d1e5470ff35df7
charlesphu <charlesphu18@gmail.com>
2025-06-30 23:39:36 -0700

    animations for all text

--- src/app/components/aboutMe.js
+++ src/app/components/aboutMe.js
@@ -9,35 +9,89 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function AboutMe() {
-  const fadeRef = useRef(null);
+  const introRef = useRef(null);
+  const introDescRef = useRef(null);
+  const skillsRef = useRef(null);
+  const skillsDescRef = useRef(null);
 
   useGSAP(
     () => {
-      gsap.fromTo(
-        fadeRef.current,
-        { opacity: 0, x: -40 },
-        {
-          x: 0,
-          opacity: 1,
-          duration: 3,
-          scrollTrigger: {
-            trigger: fadeRef.current,
-            start: "top bottom",
-            toggleActions: "play none none reverse",
-          },
-        }
-      );
+      gsap.from(introRef.current, {
+        x: -75,
+        opacity: 0,
+        duration: 2,
+        scrollTrigger: {
+          trigger: introRef.current,
+          start: "top bottom",
+          toggleActions: "play reset restart reset",
+        },
+      });
     },
-    { scope: fadeRef }
+    { scope: introRef }
+  );
+
+  useGSAP(
+    () => {
+      gsap.from(introDescRef.current, {
+        x: 100,
+        opacity: 0,
+        duration: 2,
+        scrollTrigger: {
+          trigger: introDescRef.current,
+          start: "top bottom",
+          toggleActions: "play reset restart reset",
+        },
+      });
+    },
+    { scope: introDescRef }
+  );
+
+  useGSAP(
+    () => {
+      gsap.from(skillsRef.current, {
+        x: -75,
+        opacity: 0,
+        duration: 3,
+        delay: 0.25,
+        scrollTrigger: {
+          trigger: skillsRef.current,
+          start: "top bottom",
+          toggleActions: "play reset restart reset",
+        },
+      });
+    },
+    { scope: skillsRef }
+  );
+
+  useGSAP(
+    () => {
+      gsap.from(skillsDescRef.current, {
+        x: 100,
+        opacity: 0,
+        duration: 3,
+        delay: 0.25,
+        scrollTrigger: {
+          trigger: skillsDescRef.current,
+          start: "top bottom",
+          toggleActions: "play reset restart reset",
+        },
+      });
+    },
+    { scope: skillsDescRef }
   );
 
   return (
     <div className="h-screen bg-[#CFD0DD] flex flex-col justify-center items-center">
       <Box width="80%" margin="auto">
-        <Typography ref={fadeRef} variant="h2" paddingTop={4}>
+        <Typography ref={introRef} variant="h2" paddingTop={4}>
           A Little Intro:
         </Typography>
-        <Typography variant="h5" paddingTop={2} paddingBottom={4}>
+        <Typography
+          ref={introDescRef}
+          variant="h5"
+          paddingTop={2}
+          paddingBottom={4}
+        >
           I graduated high school at 16 with the highest cumulative GPA and am
           on track to earn my bachelor’s degree from UCSC at 20—reflecting my
           discipline, enthusiasm, and relentless drive for self-improvement. I
@@ -47,10 +101,10 @@ export default function AboutMe() {
           contribute to your team. Looking forward to the possibility of working
           together!
         </Typography>
-        <Typography variant="h2" paddingBottom={2}>
+        <Typography ref={skillsRef} variant="h2" paddingBottom={2}>
           Skills:
         </Typography>
-        <Typography variant="h6">
+        <Typography ref={skillsDescRef} variant="h6">
           React, Next.js, JavaScript, Python, C++, SQL, Git, Supabase,
           Material-UI, Node.js, PostgreSQL, Firebase, Vercel, Docker,
           Agile/Scrum, CI/CD.
